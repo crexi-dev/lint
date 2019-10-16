113d88d9b2cdeb7da1d858f420542eebf9e826ba
Neil Soult <neil@crexi.com>
2019-10-16 16:02:57 -0700

    cleanup

---
+++ dist/lint/helpers/error.js
@@ -0,0 +1,50 @@
+"use strict";
+/* tslint:disable */
+/**
+ * @license
+ * Copyright 2016 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const shownWarnings = new Set();
+/**
+ * Used to exit the program and display a friendly message without the callstack.
+ */
+class FatalError extends Error {
+    constructor(message, innerError) {
+        super(message);
+        this.message = message;
+        this.innerError = innerError;
+        this.name = FatalError.NAME;
+        // Fix prototype chain for target ES5
+        Object.setPrototypeOf(this, FatalError.prototype);
+    }
+}
+FatalError.NAME = "FatalError";
+exports.FatalError = FatalError;
+function isError(possibleError) {
+    return possibleError != undefined && possibleError.message !== undefined;
+}
+exports.isError = isError;
+function showWarningOnce(message) {
+    if (!shownWarnings.has(message)) {
+        console.warn(message);
+        shownWarnings.add(message);
+    }
+}
+exports.showWarningOnce = showWarningOnce;
+function showRuleCrashWarning(message, ruleName, fileName) {
+    console.warn(`The '${ruleName}' rule threw an error in '${fileName}':\n${message}`);
+}
+exports.showRuleCrashWarning = showRuleCrashWarning;
